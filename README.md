5242585202aba5216fa113f0d98fa0ae21b975cc
Summer <summer52059@163.com>
2018-09-18 15:39:26 +0800

    vur-tree-v1.0

--- README.md
+++ README.md
@@ -1,2 +1,100 @@
 # vue-tree
-基于vue+elementUI基础写的横向树形组件
+
+> A simple tree component for vue.js
+>
+
+## 演示
+
+A [Demo](https://jiaxincui.github.io/vue-tree/dist/)
+
+## 项目跑起来,输入网址http://localhost:8080/#/
+
+```
+# install
+npm install
+npm run dev
+
+```
+
+## 数据规范
+
+`treeData`
+
+- `id` 必要属性，类型 `Number`
+
+- `label` 必要属性，类型 `String`，可自定义，默认 `label`，如： `options.label:'some_field'`
+
+- `children` 非必要，类型 `Array`
+
+### treeData示例
+./tree.json
+```json
+ 其中，child即使是空数组，也要存在，并且，id不能重复
+ treeData:[
+        {
+          "id": 1,
+          "name": "优订购代发",
+          "child": [
+            {
+              "id": 34,
+              "name": "sea仓库商品管理",
+              "child": []
+            }
+          ]
+        }
+ ]
+```
+
+## 使用示例
+    `App.vue`
+
+      ```vue
+      <template>
+        <div id="app">
+           <tree
+                :treeData="treeData"
+                v-model="ids"
+                :withoutParents="withoutParents"
+                :options="treeOptions"
+              ></tree>
+      </template>
+
+      <script>
+      import tree from '@/components/tree'
+
+      export default {
+        name: 'app',
+        components: {  'tree':tree },
+        data () {
+          return {
+             //包含父节点,必填项
+             ids:[400],
+              //不包含父节点，非必填项，当treeOptions对象中的withParents为false必须填,
+              //而且初始数值跟ids一样
+             withoutParents:[400],
+              //treeOptions非必填项
+             treeOptions:{
+              //是否包含父节点，如果为false,则要添加withoutParents这个参数，如果为true则不需要添加
+               withParents:false
+             },
+             数据结构在上面
+             treeData:[]
+          }
+        }
+      }
+      </script>
+      ```
+
+## 设置选项
+
+### treeOptions设置说明
+
+|名称|类型|默认值|说明|
+|----|----|----|----|
+|`withParents`|`Boolean`|`true`| 当为true时则不需要填写withoutParents参数，当为false时，withoutParents初始值要跟ids一样|
+
+> -- 注意：本组件采用vue+elementUI，使用的是checkbox的样式
+
+
+## License
+[MIT](https://github.com/jiaxincui/vue-tree/blob/master/LICENSE.md) © [JiaxinCui](https://github.com/jiaxincui)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="hello">
+    <tree
+      :treeData="treeData"
+      v-model="ids"
+      :withoutParents="withoutParents"
+      :options="treeOptions"
+    ></tree>
+
+
+    <div style="margin-top: 100px;">
+      包含父节点的数组用这个ids[必填项]:{{ids}}
+    </div>
+    <div>
+      不包含父节点的数组用这个withoutParents[非必填项]:{{withoutParents}}
+    </div>
+  </div>
+</template>
+
+<script>
+import tree from '@/components/tree'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return{
+      ids:[400], //包含父节点
+      withoutParents:[400],  //不包含父节点
+      treeData:[
+        {
+          "id": 1,
+          "parentId": 0,
+          "name": "优订购代发",
+          "child": [
+            {
+              "id": 34,
+              "parentId": 1,
+              "name": "sea仓库商品管理",
+              "child": []
+            },
+            {
+              "id": 343,
+              "parentId": 1,
+              "name": "sea仓库商品管理",
+              "child": []
+            },
+            {
+              "id": 31,
+              "parentId": 1,
+              "name": "sea仓库门店管理",
+              "child": [
+                {
+                  "id": 33,
+                  "parentId": 31,
+                  "name": "sea仓库财务管理",
+                  "child": []
+                },
+                {
+                  "id": 32,
+                  "parentId": 31,
+                  "name": "sea仓库门店管理类表",
+                  "child": [
+                    {
+                      "id": 400,
+                      "parentId": 32,
+                      "name": "sea11111111",
+                      "child": []
+                    },
+                  ]
+                }
+
+              ]
+            },
+
+            {
+              "id": 35,
+              "parentId": 1,
+              "name": "sea仓库订单管理",
+              "child": [
+                {
+                  "id": 38,
+                  "parentId": 35,
+                  "name": "sea仓库代发管理",
+                  "child": [
+                    {
+                      "id": 60,
+                      "parentId": 35,
+                      "name": "sea仓库代发管理",
+                      "child": []
+                    },
+                    {
+                      "id": 61,
+                      "parentId": 35,
+                      "name": "sea仓库代发管理",
+                      "child": []
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "id": 39,
+              "parentId": 1,
+              "name": "sea仓库个人中心",
+              "child": [
+                {
+                  "id": 40,
+                  "parentId": 39,
+                  "name": "sea仓库个人信息",
+                  "child": []
+                },
+                {
+                  "id": 41,
+                  "parentId": 39,
+                  "name": "sea仓库账号安全",
+                  "child": []
+                },
+                {
+                  "id": 42,
+                  "parentId": 39,
+                  "name": "sea仓库消息通知",
+                  "child": []
+                },
+                {
+                  "id": 43,
+                  "parentId": 39,
+                  "name": "sea仓库支付管理",
+                  "child": []
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      treeOptions:{
+        withParents:false  //是否包含父节点，如果为false,则要添加withoutParents这个参数，如果为true则不需要添加
+      }
+    }
+  },
+  components: {
+    'tree':tree
+  }
+}
+</script>
+
+<style>
+  li{list-style:none;vertical-align:top;}
+</style>
